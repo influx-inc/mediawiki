d506c30e126779570b210c381ec9838daf15b213
Andrew Markham <andrew.markham@influx.com>
2022-05-24 17:45:19 +1000

    Redis config

--- LocalSettings.php
+++ LocalSettings.php
@@ -75,8 +75,8 @@ $redis = parse_url($_ENV["REDIS_URL"]);
 
 $wgObjectCaches['redis'] = [
 	'class'                => 'RedisBagOStuff',
-	'servers'              => [ $redis['host'] . ':' . $redis['port'] ]
-	// 'password'          	 => $redis['pass']
+	'servers'              => [ $redis['host'] . ':' . $redis['port'] ],
+	'password'          	 => $redis['pass']
 	// 'connectTimeout'    => 1,
 	// 'persistent'        => false,
 	// 'automaticFailOver' => true,
